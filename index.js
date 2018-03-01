93c3526f518c4ae551b7976686958ab14db4c6c5
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-03-01 14:45:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
   // var newObject = Object.assign(object, {key : value});
   object[key] = value;
   
-  return newObject;
+  return object;
 }
